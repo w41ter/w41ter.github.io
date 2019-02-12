6ebe63c610d6dc778f74d1d1d050969f1c6c0d61
Patrick Nicholas <patricknicholas@foxmail.com>
2019-02-12 09:54:47 +0800

    update cors server

--- source/gitment/dist/gitment.browser.js
+++ source/gitment/dist/gitment.browser.js
@@ -3413,7 +3413,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://cors.wenjunjiang.win/', {
+      _utils.http.post('https://cors.hashcoding.net:8080/', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
